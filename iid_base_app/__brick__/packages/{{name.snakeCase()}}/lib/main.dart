0a25f7ed412cf6dae73caa716dd73f533ac43a6d
Anton Borries <anton.borries@2denker.de>
2022-09-02 10:09:48 +0200

    Add Brick for Base App

---
+++ iid_base_app/__brick__/packages/{{name.snakeCase()}}/lib/main.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/widgets.dart';
+import 'package:{{name.snakeCase()}}/src/{{name.snakeCase()}}_app.dart';
+
+void main() {
+  runApp(const {{name.pascalCase()}}App());
+}
---
+++ iid_base_app/__brick__/packages/{{name.snakeCase()}}/lib/src/{{name.snakeCase()}}_app.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+import 'package:{{name.snakeCase()}}/src/screens/home_screen.dart';
+
+class {{name.pascalCase()}}App extends StatelessWidget {
+  const {{name.pascalCase()}}App({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        body: const HomeScreen(),
+      ),
+    );
+  }
+}
