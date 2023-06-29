c1069e487abe8a2679fd6f158e28ae069ae2e09b
Anton Borries <anton.borries@2denker.de>
2023-06-29 15:41:47 +0200

    Fix Lints

--- bricks/iid_base_app/__brick__/packages/{{name.snakeCase()}}/lib/src/screens/home_screen.dart
+++ bricks/iid_base_app/__brick__/packages/{{name.snakeCase()}}/lib/src/screens/home_screen.dart
@@ -5,8 +5,10 @@ class HomeScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Center(child: const Text('{{name.pascalCase()}} - Home'))
+    return const Scaffold(
+      body: Center(
+        child: Text('{{name.pascalCase()}} - Home'),
+      ),
     );
   }
 }
