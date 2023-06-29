f3bfa7be66c151198ea5744236caac728368bf57
Anton Borries <anton.borries@2denker.de>
2023-06-29 15:48:26 +0200

    Select Platforms from Array

--- bricks/iid_base_app/hooks/pre_gen.dart
+++ bricks/iid_base_app/hooks/pre_gen.dart
@@ -4,17 +4,44 @@ import 'package:mason/mason.dart';
 
 Future<void> run(HookContext context) async {
   await Process.run('mkdir', ['packages']);
-  
+
   final org = context.vars['orgId'];
   final name = (context.vars['name'] as String).snakeCase;
 
-
   final flutterProgress = context.logger.progress('Creating Flutter App');
-  await Process.run('flutter', ['create', '--org=$org', name], workingDirectory: 'packages');
-  await Process.run('rm', ['-rf', 'main.dart'], workingDirectory: 'packages/$name/lib');
-  await Process.run('rm', ['-rf', 'analysis_options.yaml'], workingDirectory: 'packages/$name');
-  await Process.run('rm', ['-rf', 'widget_test.dart'], workingDirectory: 'packages/$name/test');
-  await Process.run('flutter', ['pub', 'add', 'provider'], workingDirectory: 'packages/$name');
-  await Process.run('flutter', ['pub', 'add', 'mocktail', '--dev'], workingDirectory: 'packages/$name');
-  flutterProgress.complete('Created Flutter App');
+
+  final platforms = (context.vars['platforms'] as Iterable).join(',');
+
+  await Process.run(
+    'flutter',
+    [
+      'create',
+      '--platforms=$platforms',
+      '--org=$org',
+      name,
+    ],
+    workingDirectory: 'packages',
+  );
+  await Process.run('rm', ['-rf', 'main.dart'],
+      workingDirectory: 'packages/$name/lib');
+  await Process.run(
+    'rm',
+    ['-rf', 'analysis_options.yaml'],
+    workingDirectory: 'packages/$name',
+  );
+  await Process.run(
+    'rm',
+    ['-rf', 'widget_test.dart'],
+    workingDirectory: 'packages/$name/test',
+  );
+  await Process.run(
+    'flutter',
+    ['pub', 'add', 'provider'],
+    workingDirectory: 'packages/$name',
+  );
+  await Process.run('flutter', ['pub', 'add', 'mocktail', '--dev'],
+      workingDirectory: 'packages/$name');
+  flutterProgress.complete(
+    'Created Flutter App',
+  );
 }
