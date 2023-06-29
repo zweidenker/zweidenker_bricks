9c644d4319cfef9e199179c917505750a0baa138
Anton Borries <anton.borries@2denker.de>
2023-06-29 15:32:19 +0200

    Include Golden Setup in BaseApp

--- bricks/iid_base_app/__brick__/packages/{{name.snakeCase()}}/test/src/screens/home_screen_test.dart
+++ bricks/iid_base_app/__brick__/packages/{{name.snakeCase()}}/test/src/screens/home_screen_test.dart
@@ -1,19 +1,43 @@
+import 'package:flutter/widgets.dart';
 import 'package:flutter_test/flutter_test.dart';
 
 import 'package:{{name.snakeCase()}}/src/screens/home_screen.dart';
+import 'package:alchemist/alchemist.dart';
+import 'package:golden_toolkit/golden_toolkit.dart' show loadAppFonts;
 import '../../tool/mocks.dart';
 import '../../tool/test_app.dart';
 
 void main() {
-  testWidgets('HomeScreen Displays', (tester) async {
-    final repository = MockRepository();
-    final target = TestApp(
-      repository: repository,
-      child: HomeScreen(),
-    );
+  group('Golden Tests', () {
+    setUpAll(() async {
+      await loadAppFonts();
+    });
 
-    await tester.pumpWidget(target);
-    await tester.pump();
-    expect(find.text('{{name.pascalCase()}} - Home'), findsOneWidget);
+    goldenTest(
+      'HomeScreen',
+      fileName: 'home-screen',
+      constraints: const BoxConstraints(maxHeight: 955, maxWidth: 485),
+      pumpBeforeTest: precacheImages,
+      builder: () {
+        return TestApp(
+          repository: MockRepository(),
+          child: const HomeScreen(),
+        );
+      },
+    );
+  });
+
+  group('Widget Tests', () {
+    testWidgets('HomeScreen Displays', (tester) async {
+      final repository = MockRepository();
+      final target = TestApp(
+        repository: repository,
+        child: const HomeScreen(),
+      );
+
+      await tester.pumpWidget(target);
+      await tester.pump();
+      expect(find.text('{{name.pascalCase()}} - Home'), findsOneWidget);
+    });
   });
 }
